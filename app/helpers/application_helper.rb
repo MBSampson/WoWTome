80a5cfea1cd1936e023eb643d7705427eedd647f
Matthew Sampson <mbsampson89@gmail.com>
2018-09-17 15:52:31 -0700

    Implements navbar and proper routing for nav items

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def display_authentication_options
+    content_tag :ul, class: 'navbar-nav' do
+      if current_user
+        concat link_to 'Settings', edit_user_registration_url, class: 'nav-link'
+        concat link_to('Sign Out', destroy_user_session_url, method: :delete, class: 'nav-link')
+      else
+        concat link_to 'Create Account', new_user_registration_url, class: 'nav-link'
+        concat link_to('Sign In', new_user_session_url, class: 'nav-link')
+      end
+    end
+  end
 end
