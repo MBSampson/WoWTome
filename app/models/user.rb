8b1365bd977324a196c17c1dd512a146d4e997a0
Matthew Sampson <mbsampson89@gmail.com>
2018-09-18 14:59:23 -0700

    AccountName/Email login capability

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,29 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :characters
+
+  attr_writer :login
+
+  validates :email, uniqueness: true
+  validates :account_name, presence: :true, uniqueness: { case_sensitive: false }
+  validate :validate_username
+
+  def login
+    @login || self.account_name || self.email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["lower(account_name) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:account_name) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
+  end
+
+  def validate_username
+    if User.where(email: account_name).exists?
+      errors.add(:account_name, :invalid)
+    end
+  end
 end
