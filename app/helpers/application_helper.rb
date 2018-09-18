84f6c700a80ee179e65f6010f4b8dcd126e3f5ac
Matthew Sampson <mbsampson89@gmail.com>
2018-09-18 13:06:01 -0700

    Modify styles across app

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,10 +3,10 @@ module ApplicationHelper
     content_tag :ul, class: 'navbar-nav' do
       if current_user
         concat link_to 'Settings', edit_user_registration_url, class: 'nav-link'
-        concat link_to('Sign Out', destroy_user_session_url, method: :delete, class: 'nav-link')
+        concat link_to('logout', destroy_user_session_url, method: :delete, class: 'nav-link')
       else
         concat link_to 'Create Account', new_user_registration_url, class: 'nav-link'
-        concat link_to('Sign In', new_user_session_url, class: 'nav-link')
+        concat link_to('Login', new_user_session_url, class: 'nav-link')
       end
     end
   end
