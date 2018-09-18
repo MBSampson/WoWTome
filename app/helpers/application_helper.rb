5482412a801222a0fcb91037609152c34c98581a
Matthew Sampson <mbsampson89@gmail.com>
2018-09-18 13:46:15 -0700

    Restyled app to use dark BG

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
     content_tag :ul, class: 'navbar-nav' do
       if current_user
         concat link_to 'Settings', edit_user_registration_url, class: 'nav-link'
-        concat link_to('logout', destroy_user_session_url, method: :delete, class: 'nav-link')
+        concat link_to('Logout', destroy_user_session_url, method: :delete, class: 'nav-link')
       else
         concat link_to 'Create Account', new_user_registration_url, class: 'nav-link'
         concat link_to('Login', new_user_session_url, class: 'nav-link')
