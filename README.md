69c441aa22c4dfbe0a85c0926bf128c398e1eff8
Matthew Sampson <mbsampson89@gmail.com>
2018-09-18 15:22:06 -0700

    Implements ability to sign in with acc name or email

--- README.md
+++ README.md
@@ -3,8 +3,11 @@
 - Configure devise for production: "in production, :host should be set to the actual host of your application"
 - Select 2 - 3 Google fonts for website headers, text, buttons, and title texts
 - Address devise error messages - can we highlight the input fields instead of generating a giant alert block? If not then consider relocating error messages to the right side of the view's `container` div.
-
+- Style Forgotten Password devise view
+- Install LetterOpener to view and style application emails
 
 # On-going
 - Utilize FactoryGirl & rspec to create comprehensive tests
 
+# Documentation
+- [Sign in with email or account_name](https://github.com/plataformatec/devise/wiki/How-To:-Allow-users-to-sign-in-using-their-username-or-email-address)
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,22 +9,26 @@ class User < ApplicationRecord
 
   validates :email, uniqueness: true
   validates :account_name, presence: :true, uniqueness: { case_sensitive: false }
-  validate :validate_username
+  validate :validate_account_name
 
   def login
     @login || self.account_name || self.email
   end
 
-  def self.find_for_database_authentication(warden_conditions)
+  def self.find_first_by_auth_conditions(warden_conditions)
     conditions = warden_conditions.dup
     if login = conditions.delete(:login)
-      where(conditions.to_h).where(["lower(account_name) = :value OR lower(email) = :value", { :value => login.downcase }]).first
-    elsif conditions.has_key?(:account_name) || conditions.has_key?(:email)
-      where(conditions.to_h).first
+      where(conditions).where(["lower(account_name) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      if conditions[:account_name].nil?
+        where(conditions).first
+      else
+        where(account_name: conditions[:account_name]).first
+      end
     end
   end
 
-  def validate_username
+  def validate_account_name
     if User.where(email: account_name).exists?
       errors.add(:account_name, :invalid)
     end
