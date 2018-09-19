73be2cd1b463abc27ae3aa4d14473e03f0d223af
Matthew Sampson <mbsampson89@gmail.com>
2018-09-19 15:21:55 -0700

    Add defaults to health and calc stats

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,14 +1,23 @@
 class Character < ApplicationRecord
   belongs_to :user
 
-  validates_presence_of :user_id
-  validates_numericality_of :level, :health, :stamina, :strength, :spirit, :agility, :intelligence, :gold, :achievement_points, greater_than: 0
+  validates_presence_of :user_id, :health, :gold, :achievement_points
+  validates_numericality_of :level, :stamina, :strength, :spirit, :agility, :intelligence, greater_than: 0
   validates :name, presence: true, uniqueness: true
 
-  before_save :sanitize_fields
+  before_save :sanitize_fields, :calculate_stats
 
   def sanitize_fields
     name.downcase!
     location.downcase!
+  end
+
+  def calculate_stats
+    self.stamina = self.stamina * self.level
+    self.strength = self.strength * self.level
+    self.spirit = self.spirit * self.level
+    self.agility = self.agility * self.level
+    self.intelligence = self.intelligence * self.level
+    self.health = self.stamina * 10
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180919215119) do
+ActiveRecord::Schema.define(version: 20180919221429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20180919215119) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.integer "level"
-    t.integer "health"
+    t.integer "health", default: 0
     t.string "location"
     t.integer "stamina", default: 5
     t.integer "strength", default: 5
