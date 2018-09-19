e9a9612b0ee5cb9df804d9de01b83b822969b864
Matthew Sampson <mbsampson89@gmail.com>
2018-09-19 15:53:48 -0700

    Rspec test for character calculate_stats

--- spec/factories/character_factory.rb
+++ spec/factories/character_factory.rb
@@ -4,14 +4,14 @@ FactoryGirl.define do
     sequence :name do |i|
       "#{Faker::Name.first_name} #{i}"
     end
-    level { Faker::Number.between(1, 60) }
-    health { Faker::Number.between(1, 5000) }
+    level 20
+    health 0
     location { Faker::Lorem.word  }
-    stamina { Faker::Number.between(5, 300) }
-    strength { Faker::Number.between(5, 300) }
-    spirit { Faker::Number.between(5, 300) }
-    agility { Faker::Number.between(5, 300) }
-    intelligence { Faker::Number.between(5, 300) }
+    stamina 5
+    strength 5
+    spirit 5
+    agility 5
+    intelligence 5
     gold { Faker::Number.between(0, 50000) }
     achievement_points { Faker::Number.between(0, 4000) }
   end
--- spec/models/character_spec.rb
+++ spec/models/character_spec.rb
@@ -10,4 +10,22 @@ describe Character, :type => :model do
       expect(/[[:upper:]]/.match(character.location)).to be_nil
     end
   end
+
+  context '#calculate_stats' do
+    it 'has properly calculated stats (level + stat)' do
+      base_stat_value = 5
+      stamina_multiplier = 10
+      character = build(:character)
+      stat_value = base_stat_value + character.level
+      character.save!
+
+      expect(character.stamina).to eq(stat_value)
+      expect(character.strength).to eq(stat_value)
+      expect(character.spirit).to eq(stat_value)
+      expect(character.agility).to eq(stat_value)
+      expect(character.intelligence).to eq(stat_value)
+
+      expect(character.health).to eq(stamina_multiplier * character.stamina)
+    end
+  end
 end
