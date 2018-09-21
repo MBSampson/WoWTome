fe0909891f4032bd8b17dee5d1146c9bd58aa5e6
Matthew Sampson <mbsampson89@gmail.com>
2018-09-21 11:36:02 -0700

    Refactor hideSpec

--- app/assets/javascripts/class_specializations.js
+++ app/assets/javascripts/class_specializations.js
@@ -35,35 +35,30 @@ function hideSpecs(spec_select, index) {
   spec_options = $(spec_select).find('option');
   resetSpecs(spec_options);
 
-  // test comment
-  var bounds = (index + 1) * 3;
-  var upper_bounds = bounds;
-  var lower_bounds = bounds - 3;
+  //
+  var cursor_head = (index + 1) * 3;
+  var cursor_tail = cursor_head - 3;
 
-  console.log('i:', index);
-  console.log('lower', lower_bounds);
-  console.log('upper', upper_bounds);
-
-  if(upper_bounds == 3) {
+  if(cursor_head == 3) {
     for (var i = 0; i < spec_options.length; i++) {
-      if (i >= upper_bounds) {
-        $(spec_options).eq(lower_bounds).attr("selected", true).change();
+      if (i >= cursor_head) {
+        $(spec_options).eq(cursor_tail).attr("selected", true).change();
         $(spec_options).eq(i).hide();
       }
     }
-  } else if (upper_bounds > 4 && upper_bounds < 25) {
+  } else if (cursor_head > 4 && cursor_head < 25) {
     for(var i = 0; i < spec_options.length; i++) {
-      if (i == lower_bounds) {
-        $(spec_options).eq(lower_bounds).attr("selected", true).change();
+      if (i == cursor_tail) {
+        $(spec_options).eq(cursor_tail).attr("selected", true).change();
       }
-      if (i < lower_bounds || i >= upper_bounds) {
+      if (i < cursor_tail || i >= cursor_head) {
         $(spec_options).eq(i).hide();
       }
     }
   } else {
     for(var i = 0; i < spec_options.length; i++) {
-      if (i < lower_bounds) {
-        $(spec_options).eq(lower_bounds).attr("selected", true).change();
+      if (i < cursor_tail) {
+        $(spec_options).eq(cursor_tail).attr("selected", true).change();
         $(spec_options).eq(i).hide();
       }
     }
