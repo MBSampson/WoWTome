858682caa03880206025b5fa9a024703023f6b05
Matthew Sampson <mbsampson89@gmail.com>
2018-09-25 15:58:29 -0700

    Change create to build

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe Post, type: :model do
   context '#show_author' do
     it "should return the author's account name" do
-      post = create(:post)
+      post = build(:post)
       expect(post.show_author).to eq(post.user.account_name)
     end
   end
