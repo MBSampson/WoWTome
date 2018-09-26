fc8c2ea68976da9f364e33dc07990bb5da7d5b1b
Matthew Sampson <mbsampson89@gmail.com>
2018-09-26 15:08:42 -0700

    user spec refactor

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,20 +3,24 @@
 require 'rails_helper'
 
 describe User, :type => :model do
+  describe 'User' do
     context 'has a character' do
-    it 'appropriately links a user and character' do
-      character = create(:character)
-      expect(character.user).to_not be_nil
+      let(:user) { create(:user, :with_character) }
+      it 'should return the user when referenced' do
+        expect(user.characters.first).to be_a Character
+      end
+    end
+
+    context '#sanitize_fields' do
+      let(:user) { create(:user, email: 'TEST@TEST.COM', account_name: 'fireTruck') }
+      it 'should downcase email and account_name' do
+        expect(/[[:upper:]]/.match(user.email)).to be_nil
+        expect(/[[:upper:]]/.match(user.account_name)).to be_nil
+      end
     end
   end
 
-  context '#sanitize_fields' do
-    it 'downcases email and account_name' do
-      user = build(:user, email: 'TEST@TEST.COM', account_name: 'TEST', password: 'password')
-      user.sanitize_fields
-
-      expect(/[[:upper:]]/.match(user.email)).to be_nil
-      expect(/[[:upper:]]/.match(user.account_name)).to be_nil
-    end
+  describe 'Admin' do
+    # TODO: Implement admins
   end
 end
