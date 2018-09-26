222c6ae66de4a7592841dedfa48163750a00978c
Matthew Sampson <mbsampson89@gmail.com>
2018-09-26 10:29:55 -0700

    Add root path rspec controller test

--- spec/controllers/tome_controller_spec.rb
+++ spec/controllers/tome_controller_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe TomeController, type: :controller do
-
+describe TomeController, type: :controller do
+  it 'navigates to the root path and ensures that unauthenticated users are taken to the login page' do
+    visit root_path
+    expect(page).to have_content('Login')
+  end
 end
