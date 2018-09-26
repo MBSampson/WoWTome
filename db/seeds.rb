77c96c84d3e6b9bd799104b3ab315412a6d83e42
Matthew Sampson <mbsampson89@gmail.com>
2018-09-26 12:58:38 -0700

    User character seed and README update

--- db/seeds.rb
+++ db/seeds.rb
@@ -81,8 +81,32 @@ user = User.create!(
 )
 user.save!
 
+user2 = User.create!(
+  email:'mrourke@test.com',
+  account_name: 'mrou',
+  password: '123123',
+  password_confirmation: '123123'
+)
 
+user2.save!
 puts 'Generated User seeds'
+
+user.characters.create!(
+  name: 'vallihn',
+  level: 60,
+  health: 100,
+  location: 'stormwind',
+  stamina: 5,
+  strength: 5,
+  spirit: 5,
+  agility: 5,
+  intelligence: 5,
+  gold: 1000,
+  achievement_points: 1280,
+  character_class_id: 4,
+  spec_id: 2
+)
+puts 'Generated User Characters'
 
 
 
