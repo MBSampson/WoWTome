f5d839728d97a26c9595f00b2cd81702c2afed45
Matthew Sampson <mbsampson89@gmail.com>
2018-09-26 12:07:40 -0700

    Fix user factory, implement get-index test

--- spec/controllers/tome_controller_spec.rb
+++ spec/controllers/tome_controller_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
 describe TomeController, type: :controller do
-  context 'unauthenticated user' do
-    it 'tries to visit Tome#index' do
-      visit tome_index_path
-      expect(page).to have_content('You need to sign in or sign up before continuing.')
+  describe 'GET #index' do
+    context 'unauthenticated user' do
+      it 'tries to visit Tome#index' do
+        visit tome_index_path
+        expect(page).to have_content('You need to sign in or sign up before continuing.')
+      end
     end
   end
 end
