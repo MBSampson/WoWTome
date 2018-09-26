0aee2481f5c705834a4bac4074aa02bd821f709b
Matthew Sampson <mbsampson89@gmail.com>
2018-09-26 14:52:10 -0700

    character spec refactor

--- spec/models/character_spec.rb
+++ spec/models/character_spec.rb
@@ -1,42 +1,48 @@
 require 'rails_helper'
 
 describe Character, :type => :model do
-  context '#sanitize_fields' do
-    it 'downcases name and location' do
-      character = build(:character)
-      character.save!
-
-      expect(/[[:upper:]]/.match(character.name)).to be_nil
-      expect(/[[:upper:]]/.match(character.location)).to be_nil
+  describe '#sanitize_fields' do
+    context 'a new character is created' do
+      let(:character) { create(:character, name: 'DWIGHT', location: 'DUNDERMIFFLIN') }
+      it 'should downcase the name and location after save' do
+        expect(/[[:upper:]]/.match(character.name)).to be_nil
+        expect(/[[:upper:]]/.match(character.location)).to be_nil
+      end
     end
   end
 
-  context '#calculate_stats' do
-    it 'has properly calculated stats (level + stat)' do
-      character = create(:character)
-      base_stat_value = 5
-      stat_value = base_stat_value + character.level
-      stamina_multiplier = 10
-      
-      expect(character.stamina).to eq(stat_value)
-      expect(character.strength).to eq(stat_value)
-      expect(character.spirit).to eq(stat_value)
-      expect(character.agility).to eq(stat_value)
-      expect(character.intelligence).to eq(stat_value)
+  describe '#calculate_stats' do
+    context 'a new character is created' do
+      let(:character) { create(:character) }
+      it "should assign a character's stats & health based upon the character's level" do
+        base_stat_value = 5
+        stat_value = base_stat_value + character.level
+        stamina_multiplier = 10
 
-      expect(character.health).to eq(stamina_multiplier * character.stamina)
+        expect(character.stamina).to eq(stat_value)
+        expect(character.strength).to eq(stat_value)
+        expect(character.spirit).to eq(stat_value)
+        expect(character.agility).to eq(stat_value)
+        expect(character.intelligence).to eq(stat_value)
+
+        expect(character.health).to eq(stamina_multiplier * character.stamina)
+      end
     end
   end
 
-  context '#show_spec' do
-    it "returns a character's specialization" do
-      arms_warrior = create(:character, :arms_warrior)
-      feral_druid = create(:character, :feral_druid)
-      frost_mage = create(:character, :frost_mage)
+  describe '#show_spec' do
+    context 'character is a feral druid' do
+      let(:feral_druid) { create(:character, :feral_druid) }
+      it "should return 'feral'" do
+        expect(feral_druid.show_spec).to eq('feral')
+      end
+    end
 
-      expect(arms_warrior.show_spec).to eq('arms')
-      expect(feral_druid.show_spec).to eq('feral')
-      expect(frost_mage.show_spec).to eq('frost')
+    context 'character is an arms warrior' do
+      let(:arms_warrior) { create(:character, :arms_warrior) }
+      it "should return 'arms'" do
+        expect(arms_warrior.show_spec).to eq('arms')
+      end
     end
   end
 
